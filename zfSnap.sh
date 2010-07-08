efa129c1318f92c2dac290b47767f11e7a6786c6
Aldis Berjoza <aldis@bsdroot.lv>
2010-07-08 20:22:51 +0300

    changed format

--- zfSnap.sh
+++ zfSnap.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # beerware license, written by Aldis Berjoza (aldis@bsdroot.lv)
 
-age=2592000	# max snapshot age in seconds
+#age=2592000	# max snapshot age in seconds
+age=100	# max snapshot age in seconds
 
-tfrmt="%Y.%m.%d_%T"
+tfrmt="%Y-%m-%d_%T"
 
 [ "$1" = "-r" ] && { zopt=-r; shift; } # if arg1 = -r them make recursive snapshots
 
@@ -13,7 +14,7 @@ for i in $*; do
 done
 
 dtime=`date +%s-$age | bc -l`
-for i in `zfs list -H -t snapshot | awk '{print $1}' | grep -E -e '^.*@20[0-9]{2}\.[01][0-9]\.[0-3][0-9]_[0-2][0-9]:[0-6][0-9]:[0-6][0-9]$'`; do
+for i in `zfs list -H -t snapshot | awk '{print $1}' | grep -E -e '^.*@20[0-9]{2}-[01][0-9]-[0-3][0-9]_[0-2][0-9]:[0-6][0-9]:[0-6][0-9]$'`; do
 	[ $dtime -gt $(date -j -f $tfrmt $(echo $i | sed -e 's/^.*@//') +%s) ] && zfs destroy $i
 done
 
