2582179211825d53f31ac16a389a1b00e599d5fd
Aldis Berjoza <killasmurf86@killasmurf86-laptop.(none)>
2010-07-18 15:19:48 +0300

    added -p and -P switch

--- zfSnap.sh
+++ zfSnap.sh
@@ -1,29 +1,34 @@
 #!/bin/sh
 # beerware license, written by Aldis Berjoza (aldis@bsdroot.lv)
 
-# if arg1 is -a. snapshot max age will be set to arg2
-# all next args are zfs filesystems
-# before zfs filesystem you can pass optional arg -r, to specify
-#   that zfs should create recursive snapshots for given filesystem
+[ $# = 0 ] && cat << EOF
+Syntax:
+${0##./} [-a seconds] [-p prefix] [-P postifx] [-r] z/fs1 [[[-r] z/fs2] ...]
 
-# Syntax:
-# zfSnap.sh [-a MaxSnapshotAgeInSeconds] [-r] z/fs1 [[[-r] z/fs2] ...]
+Options:
+-a seconds   = set how long snapshot should be kept (in seconds)
+-p prefix    = prefix snapshots with prefix
+-P postifx   = postifx snapshots with postfix
+-r           = recursive snapshots
+EOF
 
-age=2592000	# default max snapshot age in seconds
-[ $1 = '-a' ] && { age=$2; shift 2; }
+age=2592000	# default max snapshot age in seconds (30 days)
+[ "$1" = '-a' ] && { age=$2; shift 2; }
+[ "$1" = '-p' ] && { prefix=$2; shift 2; } 
+[ "$1" = '-P' ] && { postfix=$2; shift 2; }
 
 tfrmt="%Y-%m-%d_%T"
 
 ntime=`date +$tfrmt`
 while [ $1 ]; do
 	[ $1 = '-r' ] && { zopt=$1; shift; } || zopt=''
-	zfs snapshot $zopt $1@$ntime
+	zfs snapshot $zopt $1@${prefix}${ntime}${postfix}
 	shift
 done
 
 dtime=`date +%s-$age | bc -l`
-for i in `zfs list -H -t snapshot | awk '{print $1}' | grep -E -e '^.*@20[0-9]{2}-[01][0-9]-[0-3][0-9]_[0-2][0-9]:[0-6][0-9]:[0-6][0-9]$'`; do
-	[ $dtime -gt $(date -j -f $tfrmt $(echo $i | sed -e 's/^.*@//') +%s) ] && zfs destroy $i
+for i in `zfs list -H -t snapshot | awk '{print $1}' | grep -E -e "^.*@${prefix}20[0-9]{2}-[01][0-9]-[0-3][0-9]_[0-2][0-9]:[0-6][0-9]:[0-6][0-9]${postfix}$"`; do
+	[ $dtime -gt $(date -j -f $tfrmt $(echo $i | sed -e "s/^.*@${prefix}//" -e "s/${postfix}$//") +%s) ] && zfs destroy $i
 done
 
 exit 0
