fbe72010337a2cc51c41174e7ddaee98b870cb29
Aldis Berjoza <aldis@bsdroot.lv>
2010-08-03 14:46:53 +0300

    Add license text. add links

--- zfSnap.sh
+++ zfSnap.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-# beerware license, written by Aldis Berjoza <aldis@bsdroot.lv>
-#    http://wiki.bsdroot.lv/zfsnap
 
-VERSION=1.3
+# "THE BEER-WARE LICENSE":
+# <aldis@bsdroot.lv> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return Aldis Berjoza
+
+# http://wiki.bsdroot.lv/zfsnap
+# http://aldis.git.bsdroot.lv/zfSnap/
+
+VERSION=1.4
 
 s2time() {
 	# convert seconds to human readable time
