2db85235f56ba9ab0de84dfb2cb530c8d89b56eb
Aldis Berjoza <aldis@bsdroot.lv>
2010-09-13 22:41:09 +0300

    fix

--- zfSnap.sh
+++ zfSnap.sh
@@ -3,7 +3,7 @@
 # "THE BEER-WARE LICENSE":
 # <aldis@bsdroot.lv> wrote this file. As long as you retain this notice you
 # can do whatever you want with this stuff. If we meet some day, and you think
-# this stuff is worth it, you can buy me a beer in return Aldis Berjoza
+# this stuff is worth it, you can buy me a beer in return. Aldis Berjoza
 
 # wiki: 			http://wiki.bsdroot.lv/zfsnap
 # repository:		http://aldis.git.bsdroot.lv/zfSnap/
