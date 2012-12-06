1ea73202310900e193b2d5c4cde895c88ba310b6
Aldis Berjoza <graudeejs@yandex.ru>
2012-12-07 00:09:44 +0200

    Update email

--- zfSnap.sh
+++ zfSnap.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 # "THE BEER-WARE LICENSE":
-# <graudeejs@gmail.com> wrote this file. As long as you retain this notice you
+# <graudeejs@yandex.com> wrote this file. As long as you retain this notice you
 # can do whatever you want with this stuff. If we meet some day, and you think
 # this stuff is worth it, you can buy me a beer in return. Aldis Berjoza
 
 # wiki:             https://github.com/graudeejs/zfSnap/wiki
 # repository:       https://github.com/graudeejs/zfSnap
 # Bug tracking:     https://github.com/graudeejs/zfSnap/issues
-# feedback email:   graudeejs@gmail.com
 
 readonly VERSION=1.10.4
 readonly zfs_cmd=/sbin/zfs
