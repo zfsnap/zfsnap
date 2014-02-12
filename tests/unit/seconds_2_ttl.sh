be45646576de920dd772d60549c8f336d0d1d25a
Alex Waite <Alexqw85@gmail.com>
2014-02-12 12:51:27 +0100

    Add missing support for "weeks" to Seconds2TTL

--- tests/unit/seconds_2_ttl.sh
+++ tests/unit/seconds_2_ttl.sh
@@ -9,9 +9,10 @@ ItEchos "Seconds2TTL 59"        "59s"
 ItEchos "Seconds2TTL 60"        "1M"
 ItEchos "Seconds2TTL 3600"      "1h"
 ItEchos "Seconds2TTL 86400"     "1d"
+ItEchos "Seconds2TTL 604800"    "1w"
 ItEchos "Seconds2TTL 2592000"   "1m"
 ItEchos "Seconds2TTL 31104000"  "12m"
 ItEchos "Seconds2TTL 31536000"  "1y"
-ItEchos "Seconds2TTL 71211967"  "2y3m4d5h6M7s"
+ItEchos "Seconds2TTL 72421567"  "2y3m2w4d5h6M7s"
 
 ExitTests
