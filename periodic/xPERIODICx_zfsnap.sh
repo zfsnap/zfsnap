18efccdaed59530053e8dc58c6dd6ef80dadad49
Alex Waite <Alexqw85@gmail.com>
2014-04-11 15:05:05 +0200

    Update periodic scripts -- untested
    I don't have access to a FreeBSD box yet, so this is untested.
    
    The majority of changes are whitespace (tabs to spaces) and comments
    fixes.
    
    The only real functional change was to update the `zfsnap` calls to the
    new syntax.

--- periodic/xPERIODICx_zfsnap.sh
+++ periodic/xPERIODICx_zfsnap.sh
@@ -6,61 +6,57 @@
 # If there is a global system configuration file, suck it in.
 #
 if [ -r /etc/defaults/periodic.conf ]; then
-	. /etc/defaults/periodic.conf
-	source_periodic_confs
+    . /etc/defaults/periodic.conf
+    source_periodic_confs
 fi
 
-# xPERIODICx_zfsnap_enable			- Enable xPERIODICx snapshots (values: YES | NO)
-# xPERIODICx_zfsnap_flags			- zfsnap generic flags (except -v and -d)
-# xPERIODICx_zfsnap_fs				- Space separated zfs filesystems to create non-recursive snapshots
-# xPERIODICx_zfsnap_recursive_fs	- Space separated zfs filesystems to create recursive snapshots
-# xPERIODICx_zfsnap_ttl				- Set Time To Live
-# xPERIODICx_zfsnap_verbose			- Verbose output (values: YES | NO)
-# xPERIODICx_zfsnap_enable_prefix	- Create snapshots with prefix (values: YES | NO) (Default = YES)
-# xPERIODICx_zfsnap_prefix			- set prefix for snapshots (Default = xPERIODICx)
+# xPERIODICx_zfsnap_enable          - Enable xPERIODICx snapshots (values: YES | NO)
+# xPERIODICx_zfsnap_flags           - `zfsnap snapshot` flags
+# xPERIODICx_zfsnap_fs              - Space-separated ZFS filesystems to create non-recursive snapshots
+# xPERIODICx_zfsnap_recursive_fs    - Space-separated ZFS filesystems to create recursive snapshots
+# xPERIODICx_zfsnap_ttl             - Explicit TTL value
+# xPERIODICx_zfsnap_verbose         - Verbose output (values: YES | NO)
+# xPERIODICx_zfsnap_enable_prefix   - Create snapshots with prefix (values: YES | NO) (Default = YES)
+# xPERIODICx_zfsnap_prefix          - set prefix for snapshots (Default = xPERIODICx)
 
 case "${xPERIODICx_zfsnap_enable-"NO"}" in
-	[Yy][Ee][Ss])
-		OPTIONS="$xPERIODICx_zfsnap_flags"
+    [Yy][Ee][Ss])
+        OPTIONS="$xPERIODICx_zfsnap_flags"
 
-		case "${xPERIODICx_zfsnap_verbose-"NO"}" in
-		[Yy][Ee][Ss])
-			OPTIONS="$OPTIONS -v"
-			;;
-		esac
+        case "${xPERIODICx_zfsnap_verbose-"NO"}" in
+            [Yy][Ee][Ss]) OPTIONS="$OPTIONS -v" ;;
+        esac
 
-		case "${xPERIODICx_zfsnap_enable_prefix-"YES"}" in
-		[Yy][Ee][Ss])
-			OPTIONS="$OPTIONS -p ${xPERIODICx_zfsnap_prefix:-"xPERIODICx-"}"
-			;;
-		esac
+        case "${xPERIODICx_zfsnap_enable_prefix-"YES"}" in
+            [Yy][Ee][Ss]) OPTIONS="$OPTIONS -p ${xPERIODICx_zfsnap_prefix:-"xPERIODICx-"}" ;;
+        esac
 
-		case 'xPERIODICx' in
-		'hourly')
-			default_ttl='3d'
-			;;
-		'daily'|'reboot')
-			default_ttl='1w'
-			;;
-		'weekly')
-			default_ttl='1m'
-			;;
-		'monthly')
-			default_ttl='6m'
-			;;
-		*)
-			echo "ERR: Unexpected error" > /dev/stderr
-			exit 1
-			;;
-		esac
+        case 'xPERIODICx' in
+            'hourly')
+                default_ttl='3d'
+                ;;
+            'daily'|'reboot')
+                default_ttl='1w'
+                ;;
+            'weekly')
+                default_ttl='1m'
+                ;;
+            'monthly')
+                default_ttl='6m'
+                ;;
+            *)
+                printf '%s\n' "ERR: Unexpected error" > /dev/stderr
+                exit 1
+                ;;
+        esac
 
-		xPREFIXx/zfsnap $OPTIONS -a ${xPERIODICx_zfsnap_ttl:-"$default_ttl"} $xPERIODICx_zfsnap_fs -r $xPERIODICx_zfsnap_recursive_fs
-		exit $?
-		;;
+        xPREFIXx/zfsnap snapshot $OPTIONS -a ${xPERIODICx_zfsnap_ttl:-"$default_ttl"} $xPERIODICx_zfsnap_fs -r $xPERIODICx_zfsnap_recursive_fs
+        exit $?
+        ;;
 
-	*)
-		exit 0
-		;;
+    *)
+        exit 0
+        ;;
 esac
 
 # vim: set ts=4 sw=4:
--- periodic/xPERIODICx_zfsnap_delete.sh
+++ periodic/xPERIODICx_zfsnap_delete.sh
@@ -6,37 +6,35 @@
 # If there is a global system configuration file, suck it in.
 #
 if [ -r /etc/defaults/periodic.conf ]; then
-	. /etc/defaults/periodic.conf
-	source_periodic_confs
+    . /etc/defaults/periodic.conf
+    source_periodic_confs
 fi
 
-# xPERIODICx_zfsnap_delete_enable	- Delete old snapshots periodicaly (values: YES | NO)
-# xPERIODICx_zfsnap_delete_flags	- zfsnap generic flags (except -v and -d)
-# xPERIODICx_zfsnap_delete_verbose	- Verbose output (values: YES | NO)
-# xPERIODICx_zfsnap_delete_prefixes	- Space separated list of prefixes of old zfsnap snapshots to delete
-#                                     'hourly-', 'daily-', 'weekly-', 'monthly-' and 'reboot-' prefixes are hardcoded
+# xPERIODICx_zfsnap_delete_enable   - Delete old snapshots periodicaly (values: YES | NO)
+# xPERIODICx_zfsnap_delete_flags    - `zfsnap destroy` flags
+# xPERIODICx_zfsnap_delete_verbose  - Verbose output (values: YES | NO)
+# xPERIODICx_zfsnap_delete_prefixes - Space-separated list of prefixes of expired zfsnap snapshots to delete
+#                                     'hourly-', 'daily-', 'weekly-', 'monthly-', and 'reboot-' prefixes are hardcoded
 
 case "${xPERIODICx_zfsnap_delete_enable-"NO"}" in
-	[Yy][Ee][Ss])
-		OPTIONS="$xPERIODICx_zfsnap_delete_flags"
+    [Yy][Ee][Ss])
+        OPTIONS="$xPERIODICx_zfsnap_delete_flags"
 
-		case "${xPERIODICx_zfsnap_delete_verbose-"NO"}" in
-		[Yy][Ee][Ss])
-			OPTIONS="$OPTIONS -v"
-			;;
-		esac
+        case "${xPERIODICx_zfsnap_delete_verbose-"NO"}" in
+            [Yy][Ee][Ss]) OPTIONS="$OPTIONS -v" ;;
+        esac
 
-		for prefix in $xPERIODICx_zfsnap_delete_prefixes; do
-			OPTIONS="$OPTIONS -p $prefix"
-		done
+        for prefix in $xPERIODICx_zfsnap_delete_prefixes; do
+            OPTIONS="$OPTIONS -p $prefix"
+        done
 
-		xPREFIXx/zfsnap -d $OPTIONS -p 'hourly-' -p 'daily-' -p 'weekly-' -p 'monthly-' -p 'reboot-'
-		exit $?
-		;;
+        xPREFIXx/zfsnap destroy $OPTIONS -p 'hourly-' -p 'daily-' -p 'weekly-' -p 'monthly-' -p 'reboot-'
+        exit $?
+        ;;
 
-	*)
-		exit 0
-		;;
+    *)
+        exit 0
+        ;;
 esac
 
 # vim: set ts=4 sw=4:
