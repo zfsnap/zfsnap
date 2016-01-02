0e79ac5128eeecc18e80ca8ed75caa1924e7d2ff
Ryan C. Thompson <rct@thompsonclan.org>
2016-01-02 12:50:11 -0500

    Add -f option for limiting snapshot frequency

--- share/zfsnap/commands/snapshot.sh
+++ share/zfsnap/commands/snapshot.sh
@@ -4,6 +4,8 @@
 # See the AUTHORS and LICENSE files for more information.
 
 PREFIX=''                           # Default prefix
+PREFIXES=''                         # Needed for searching recent snapshots
+SNAPSHOT_FREQ=''                    # Max allowed snapshot frequency
 
 # FUNCTIONS
 Help() {
@@ -16,6 +18,9 @@ ${0##*/} snapshot [ options ] zpool/filesystem ...
 OPTIONS:
   -a ttl       = How long the snapshot(s) should be kept (default: 1 month)
   -h           = Print this help and exit
+  -f age       = Maximum frequency with which snapshots should be created. (Will
+                 not create a new snapshot if an existing one is younger than
+                 this limit.)
   -n           = Dry-run. Perform a trial run with no actions actually performed
   -p prefix    = Prefix to use when naming snapshots for all ZFS file
                  systems that follow this option
@@ -41,12 +46,15 @@ EOF
 
 # main loop; get options, process snapshot creation
 while [ "$1" ]; do
-    while getopts :a:hnp:PrRsSvz OPT; do
+    while getopts :a:hf:np:PrRsSvz OPT; do
         case "$OPT" in
             a) ValidTTL "$OPTARG" || Fatal "Invalid TTL: $OPTARG"
                TTL=$OPTARG
                ;;
             h) Help;;
+            f) ValidTTL "$OPTARG" || Fatal "Invalid Duration: $OPTARG"
+               SNAPSHOT_FREQ=$OPTARG
+               ;;
             n) DRY_RUN='true';;
             p) PREFIX=$OPTARG;;
             P) PREFIX='';;
@@ -62,6 +70,9 @@ while [ "$1" ]; do
         esac
     done
 
+    # This must be set for "ValidPrefix" to work properly
+    PREFIXES=$PREFIX
+
     # discard all arguments processed thus far
     shift $(($OPTIND - 1))
 
@@ -71,6 +82,33 @@ while [ "$1" ]; do
         ! SkipPool "$1" && shift && continue
 
         CURRENT_DATE=${CURRENT_DATE:-`date "+$TIME_FORMAT"`}
+
+        if [ -n "$SNAPSHOT_FREQ" ]; then
+            # Check if there is already a recent snapshot within
+            # $SNAPSHOT_FREQ of current date
+            ZFS_SNAPSHOTS=`$ZFS_CMD list -H -o name -t snapshot -r $1` >&2 || Fatal "'$1' does not exist!"
+            # Get the earliest date when the next snapshot should be created
+            SNAPSHOT_ALLOWED_DATE=''
+            for SNAPSHOT in $ZFS_SNAPSHOTS; do
+                TrimToFileSystem "$SNAPSHOT" && [ "$RETVAL" = "$1" ] || continue
+
+                # gets and validates snapshot name
+                TrimToSnapshotName "$SNAPSHOT" && SNAPSHOT_NAME=$RETVAL || continue
+
+                TrimToDate "$SNAPSHOT_NAME" && CREATE_DATE=$RETVAL || continue
+                DatePlusTTL "$CREATE_DATE" "$SNAPSHOT_FREQ" && FREQ_EXPIRE_DATE=$RETVAL || continue
+                if [ -z "$SNAPSHOT_ALLOWED_DATE" ] ||
+                       GreaterDate $FREQ_EXPIRE_DATE $SNAPSHOT_ALLOWED_DATE; then
+                    SNAPSHOT_ALLOWED_DATE=$FREQ_EXPIRE_DATE;
+                fi
+            done
+            # If previous snapshot is too young, don't create a new
+            # one
+            if [ -n "$SNAPSHOT_ALLOWED_DATE" ] &&
+                   GreaterDate $SNAPSHOT_ALLOWED_DATE $CURRENT_DATE; then
+                shift && continue;
+            fi
+        fi
 
         ZFS_SNAPSHOT="$ZFS_CMD snapshot $ZOPT ${1}@${PREFIX}${CURRENT_DATE}--${TTL}"
         if IsFalse "$DRY_RUN"; then
