9e9f5729c97b9a6851bc6b0fdb195d8b1471faf5
Matt Simerson <matt@tnpi.net>
2016-11-26 15:26:10 -0800

    tailor for sh usage

--- tests/lint.sh
+++ tests/lint.sh
@@ -8,9 +8,9 @@ for _dir in $DIRS_WITH_SCRIPTS
 do
     for _f in $_dir/*.sh
     do
-        sh -n "$_f" || ERRS=$((ERRS + 1))
+        sh -n "$_f"
         echo "shellcheck $_f"
-        shellcheck -e SC2009,SC2039,SC2086,SC2153,SC2016,SC1004,SC2119 "$_f" || ERRS=$((ERRS + 1))
+        shellcheck -x -s sh -e SC1091,SC2009,SC2039,SC2086,SC2153,SC2016,SC1004,SC2119 $_f || ERRS=$((ERRS + 1))
     done
 done
 
@@ -18,5 +18,5 @@ if [ $ERRS -eq 0 ]; then
     exit 0
 fi
 
-echo "Lint errors detected"
+echo "Lint errors encountered"
 exit 1
