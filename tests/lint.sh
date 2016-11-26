54b6fc999209d903fa27eee5aedc2fc235309147
Matt Simerson <matt@tnpi.net>
2016-11-26 15:23:32 -0800

    exit code 0 is no errors

--- tests/lint.sh
+++ tests/lint.sh
@@ -2,14 +2,21 @@
 
 export DIRS_WITH_SCRIPTS="periodic sbin share/zfsnap share/zfsnap/commands tools"
 
-pwd
+ERRS=0
 
 for _dir in $DIRS_WITH_SCRIPTS
 do
     for _f in $_dir/*.sh
     do
-        sh -n "$_f"
+        sh -n "$_f" || ERRS=$((ERRS + 1))
         echo "shellcheck $_f"
-        shellcheck -e SC2009,SC2086,SC2153,SC2016,SC1004,SC2119 "$_f"
+        shellcheck -e SC2009,SC2039,SC2086,SC2153,SC2016,SC1004,SC2119 "$_f" || ERRS=$((ERRS + 1))
     done
 done
+
+if [ $ERRS -eq 0 ]; then
+    exit 0
+fi
+
+echo "Lint errors detected"
+exit 1
